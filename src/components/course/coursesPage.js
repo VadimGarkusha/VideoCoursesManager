348f6b41fdccfed4aecebffc0769c3e7613ec10a
Vadym <vadgig@gmail.com>
2019-01-08 16:52:52 -0500

    Created new page to Add and Update courses.

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -1,16 +1,21 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import * as courseActions from '../../actions/courseActions';
-import {bindActionCreators} from 'redux';
+import {browserHistory} from 'react-router';
 import CourseList from './CourseList';
 
 class CoursesPage extends Component{
     constructor(props, context){
         super(props, context);
+
+        this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
     }
 
     courseRow(course, index){
         return <div key={index}>{course.title}</div>;
+    }
+
+    redirectToAddCoursePage(){
+        browserHistory.push('/course');
     }
 
     render(){
@@ -19,6 +24,10 @@ class CoursesPage extends Component{
         return(
              <div>
                 <h1>Courses</h1>
+                <input type='submit'
+                    value='Add Course'
+                    className='btn btn-primary'
+                    onClick={this.redirectToAddCoursePage}/>
                 <CourseList courses = {courses}/>
             </div>
         );
@@ -26,8 +35,7 @@ class CoursesPage extends Component{
 }
 
 CoursesPage.propTypes = {
-    courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+    courses: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps){
@@ -38,7 +46,7 @@ function mapStateToProps(state, ownProps){
 
 function mapDispatchToProps(dispatch){
     return{
-        actions: bindActionCreators(courseActions, dispatch)
+        //actions: bindActionCreators(courseActions, dispatch)
     };
 }
 
